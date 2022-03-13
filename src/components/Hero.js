662241b853a60b9e25bdb6080ffb26dfebbafbc8
Bryan Santos <brymsantos@gmail.com>
2022-03-13 16:08:46 -0700

    fixed issue with anchor buttons not working (WORKING)

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,19 +3,21 @@ import '../css/Hero.css'
 
 const Hero = () => {
   return (
-    <section id = "hero">
-      <video loop autoPlay muted>
-        <source src ='./images/coins.mp4' type="video/mp4"></source>
-      </video>
-      <header>
-        <h1><span>The #1 FREE practice trading website</span></h1>
-        <h2><span>Trade for fun. Trade to practice. Trade to compete.</span></h2>
-        <span>
-          <input placeholder ="Enter Email"></input>
-          <a href='/signup'><button>Sign Up</button></a>
-        </span>
-      </header>
-    </section>
+    <div id ="hero_container">
+      <section id = "hero">
+        <video loop autoPlay muted>
+          <source src ='./images/coins.mp4' type="video/mp4"></source>
+        </video>
+        <header>
+          <h1><span>The #1 FREE practice trading website</span></h1>
+          <h2><span>Trade for fun. Trade to practice. Trade to compete.</span></h2>
+          <span>
+            <input placeholder ="Enter Email"></input>
+            <a href='/signup'><button>Sign Up</button></a>
+          </span>
+        </header>
+      </section>
+  </div>
   );
 };
 
